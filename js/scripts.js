caac949303c9fc6d418f22a8e8cbc86a4e369ba1
Hebert Soares <hebertsoaresof@gmail.com>
2023-05-26 16:15:59 -0300

    Adicionando animação no texto inicial

--- js/scripts.js
+++ js/scripts.js
@@ -51,40 +51,48 @@ document.addEventListener('DOMContentLoaded', function() {
 
   function animateText(element, text) {
     var index = 0;
-    var isDeleting = false;
+    var typingSpeed = 200; // Velocidade de digitação (ajuste conforme necessário)
+    var eraseSpeed = 80; // Velocidade de apagamento (ajuste conforme necessário)
+    var pauseBetweenAnimations = 1000; // Tempo de pausa entre as animações (ajuste conforme necessário)
   
     function type() {
-      if (index < text.length && !isDeleting) {
+      if (index < text.length) {
         element.textContent += text.charAt(index);
         index++;
+        setTimeout(type, typingSpeed);
       } else {
-        isDeleting = true;
+        setTimeout(erase, pauseBetweenAnimations);
       }
+    }
   
-      if (isDeleting) {
-        element.textContent = text.substring(0, index);
-  
-        if (index === 0) {
-          isDeleting = false;
-        }
+    function erase() {
+      if (index > 0) {
+        element.textContent = text.substring(0, index - 1);
         index--;
+        setTimeout(erase, eraseSpeed);
+      } else {
+        setTimeout(type, pauseBetweenAnimations);
       }
-  
-      var typingSpeed = isDeleting ? 100 : 200; // Velocidade de digitação e apagamento (ajuste conforme necessário)
-      setTimeout(type, typingSpeed);
     }
   
     // Inicia a animação de digitação
     type();
   }
   
-  // Obtém o elemento <h1> com a classe "error-animation"
-  var heading = document.querySelector('.error-animation');
+  // Obtém os elementos para animação
+  var heading = document.querySelector('.masthead-heading');
+  var subheading = document.querySelector('.masthead-subheading');
   
-  // Texto para animação
-  var text = 'Hebert Soares'; // Substitua pelo texto desejado
+  // Textos para animação
+  var headingText = 'Hebert Soares'; // Substitua pelo texto desejado
+  var subheadingText = 'Programador Python - Desenvolvedor Web Django'; // Substitua pelo texto desejado
   
-  // Chama a função para iniciar a animação
-  animateText(heading, text);
+  // Chama a função para iniciar a animação nos elementos
+  animateText(heading, headingText);
+  animateText(subheading, subheadingText);
+  
+  
+  
+  
   
   
